25e096819b4348997495f6b17a4627a666f2c300
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-12-10 17:20:38 -0600

    Removed absolute path from style file parameter

--- functions/ccat.fish
+++ functions/ccat.fish
@@ -1,3 +1,3 @@
 function ccat
-	source-highlight --out-format=esc --style-file=/usr/local/Cellar/source-highlight/3.1.8/share/source-highlight/esc.style --failsafe -i $argv
+	source-highlight --out-format=esc --style-file=esc.style --failsafe -i $argv
 end
