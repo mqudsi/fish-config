202c5243117e8c79fd8e92d509a1527106efc396
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-08-07 18:59:00 -0500

    Hide output of :only in man.fish nvim execution

--- functions/man.fish
+++ functions/man.fish
@@ -1,7 +1,7 @@
 function man
 	set arg $argv[0..-1]
 	if which nvim >/dev/null
-		nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -c ":Man $arg" -c ":only"
+		nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -c ":Man $arg" -c ":silent :only"
 	else
 		eval (which man) $arg
 	end
