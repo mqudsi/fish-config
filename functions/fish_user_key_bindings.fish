0dc83d534569b30dc278aa7ac261265e27a6af48
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-09-24 16:23:43 -0500

    Bind alt+del to kill-word

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,6 +2,6 @@ function fish_user_key_bindings
     fzf_key_bindings
 
     #bind alt+del/bkspc to kill words
-    bind \e\[3~ kill-word
+    bind \e\e\[3~ kill-word
     bind \e\cH backward-kill-word
 end
