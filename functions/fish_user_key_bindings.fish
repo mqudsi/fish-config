f81c2f2ac1f29f1b34c1ef433d72ac833044a76b
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-09-26 10:08:54 -0500

    Use ctrl+home/end to move to beginning/end of commandline

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -5,4 +5,7 @@ function fish_user_key_bindings
     #bind alt+del/bkspc to kill words
     bind \e\e\[3~ kill-word
     bind \e\cH backward-kill-word
+    #ctrl+home/end for beginning/end of commandline
+    bind [1\;5H beginning-of-buffer
+    bind [1\;5F end-of-buffer
 end
