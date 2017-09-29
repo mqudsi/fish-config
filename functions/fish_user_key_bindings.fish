2b86464ed7d14aa15a560aa8bdcd90a3b79c5eeb
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-09-29 14:19:39 +0200

    Bind alt+f4 to exit

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -5,4 +5,6 @@ function fish_user_key_bindings
     #bind alt+del/bkspc to kill words
     bind \e\e\[3~ kill-word
     bind \e\cH backward-kill-word
+    #bind alt+f4 to exit
+    bind [1\;3S exit
 end
