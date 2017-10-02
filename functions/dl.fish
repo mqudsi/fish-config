cb96eb1e70f91e4bfe9b4cc1e83d3951e263faf1
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-10-02 15:38:58 +0200

    Fixed file allocation in dl for aria2c

--- functions/dl.fish
+++ functions/dl.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
 function dl --description 'alias dl aria2c -j8 -x8 --min-split-size=4M --file-allocation=none'
-	aria2c -j8 -x8 --min-split-size=4M --file-allocation=none $argv;
+	aria2c -j8 -x8 --min-split-size=4M --file-allocation=trunc $argv;
 end
