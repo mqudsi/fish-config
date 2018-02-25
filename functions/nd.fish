7c71f76f2e2890661c2f97152260cf39d195a5db
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-02-25 14:17:57 -0600

    Add pd and nd aliases for prevd/nextd

---
+++ functions/nd.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function nd --description 'alias nd nextd'
+	nextd  $argv;
+end
---
+++ functions/pd.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function pd --description 'alias pd prevd'
+	prevd  $argv;
+end
