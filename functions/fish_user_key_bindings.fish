4bae592d29f11579167ca2bcd1f72c99a213f130
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-03-24 06:56:11 -0500

    Add escaped bindings

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -6,8 +6,8 @@ function fish_user_key_bindings
     bind \e\e\[3~ kill-word
     bind \e\cH backward-kill-word
     #ctrl+home/end for beginning/end of commandline
-    bind [1\;5H beginning-of-buffer
-    bind [1\;5F end-of-buffer
+    bind \e\[1\;5H beginning-of-buffer
+    bind \e\[1\;5F end-of-buffer
     #bind alt+f4 to exit
     bind \e\[1\;3S exit
 end
