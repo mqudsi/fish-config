8ecb2a44af299f33a86eaf67abebaac416c6c0b8
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-04-24 17:24:15 -0500

    neovim man viewer

--- functions/man.fish
+++ functions/man.fish
@@ -1,7 +1,9 @@
 function man
 	set arg $argv[0..-1]
 	if which nvim >/dev/null
-		nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -c ":Man $arg" -c ":silent :only"
+		# nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -c ":Man $arg" -c ":silent :only"
+		nvim -u NORC -c ":set fileignorecase" -c ":Man $argv" -c ":silent :only" \
+			-c 'nunmap <buffer> q' -c 'nmap q :q!<CR>' -c 'nmap <space> <PageDown>'
 	else
 		eval (which man) $arg
 	end
