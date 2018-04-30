afaddbaac61016b6eae03d1741f4687f2b57ea4a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-04-29 20:30:02 -0500

    [man] Check if manpage exists without launching neovim

--- functions/man.fish
+++ functions/man.fish
@@ -1,11 +1,13 @@
 function man
-	set arg $argv[0..-1]
-	if which nvim >/dev/null
-		# nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -c ":Man $arg" -c ":silent :only"
+	if not command man -w $argv >/dev/null 2>/dev/null
+		echo "man page not found!" >&2
+		return 1
+	
+	if type -q nvim
 		nvim -u NORC -c ":set fileignorecase" -c ":Man $argv" -c ":silent :only" \
 			-c 'nunmap <buffer> q' -c 'nmap q :q!<CR>' -c 'nmap <space> <PageDown>'
 	else
-		eval (which man) $arg
+		eval (which man) $argv
 	end
 end
 
