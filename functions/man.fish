f89b23e6fa05a8282bf685d6e0ad82c715b299ed
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-05-14 21:14:15 -0500

    Update neovim man wrapper

--- functions/man.fish
+++ functions/man.fish
@@ -1,13 +1,15 @@
 function man
-	if not command man -w $argv >/dev/null 2>/dev/null
-		echo "man page not found!" >&2
+	set arg $argv[0..-1]
+	if not command man -w $arg >/dev/null 2>/dev/null
+		echo "No man page is available for '$arg'" 1>&2
 		return 1
-	
+	end
+
 	if type -q nvim
-		nvim -u NORC -c ":set fileignorecase" -c ":Man $argv" -c ":silent :only" \
+		nvim -u NORC -c ":set fileignorecase" -c ":Man $arg" -c ":silent :only" \
 			-c 'nunmap <buffer> q' -c 'nmap q :q!<CR>' -c 'nmap <space> <PageDown>'
 	else
-		eval (which man) $argv
+		command man $arg
 	end
 end
 
