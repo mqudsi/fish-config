7164e5357b19a0add7c99d3f66dc4d9b3b901d9c
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-05-16 20:43:12 -0500

    Create backup of old pasteboard contents before pbedit
    The same filename is used each time, so it's a ring buffer of length 1.

--- functions/pbedit.fish
+++ functions/pbedit.fish
@@ -3,6 +3,8 @@ function pbedit
 	mkdir -p $TMPDIR
 	set -l pbtemp (mktemp)
 	pbpaste $pbtemp
+	# Create a backup of the old pasteboard contents
+	cp $pbtemp $TMPDIR/.prevpb
 	nvim $pbtemp
 	and cat $pbtemp | pbcopy
 	and rm $pbtemp # only if we succeeded above
