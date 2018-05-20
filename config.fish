78f07e0857e7981aaad413287eec2869c5ea2cc0
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-05-20 14:08:23 -0500

    Use fd for CTRL-T and ALT-C

--- config.fish
+++ config.fish
@@ -4,3 +4,12 @@ begin
 		source $AUTOJUMP_PATH
 	end
 end
+
+if type -q fd
+	# set FZF_CTRL_T_COMMAND "rg --files --no-ignore --hidden --follow -g '!{.git,node_modules}/*'"
+	# set FZF_CTRL_T_COMMAND "fd -t f -E '*.{o,swp,tlog,tmp}' -E '^~' -E '{.git,node_modules}' --ignore-file .ignore"
+	# set FZF_CTRL_T_COMMAND "fd -t f -E '*.{o,swp,tlog,tmp}' -E '^~' -E '{.git,node_modules}'"
+	# These commands are eval'd, so if we escape $dir it will be replaced with the new value
+	set FZF_CTRL_T_COMMAND "fd -t d -t f -t l '^' \$dir --follow --color=always"
+	set FZF_ALT_C_COMMAND "fd -t d '^' \$dir --follow --color=always"
+end
