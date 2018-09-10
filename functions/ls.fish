0dad10aee88bd0b17583fbb6926daf9ee932fd08
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-09-10 15:10:28 -0500

    Drop `exa` as `ls` replacement
    `exa` is horribly slow. The only reason we were using it is because `ls`
    color output is horrible on dark-colored terminals, resolved in b70b43ed
    by dropping the background color from directory records in `LS_COLORS`.

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,25 +1,3 @@
-function ls --description 'alias ls=exa'
-	if which exa 2>&1 >/dev/null
-		#exa does not support the same parameters as ls, unfortunately
-		#see https://github.com/ogham/exa/issues/284
-		if echo $argv[1] | egrep "^-[^-]" > /dev/null
-			# for i in (seq 1 (count $argv)); echo argv[$i]: $argv[$i]; end;
-			set newarg (echo $argv[1] | sed -E "s/^-([^tr \t-]*)[tr]+([a-z]*)/-\1\2s/")
-			if not string match -- $argv[1] $newarg
-				# echo newarg: $newarg
-				if test (count $argv) -gt 1
-					set newargs $newarg "modified" $argv[2..-1]
-				else
-					set newargs $newarg "modified"
-				end
-				# for i in (seq 1 (count $newargs)); echo newargs[$i]: $newargs[$i]; end;
-				set argv $newargs
-			end
-			set -g argv $argv
-		end
-		# add -g always
-		exa -g $argv
-	else
-		eval (which ls) $argv
-	end
+function ls --description 'alias ls=ls --color=auto'
+	command ls --color=auto $argv
 end
