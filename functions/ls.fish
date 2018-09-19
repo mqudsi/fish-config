078b31d14a7f47b0fb760b40e5dcc5a529534600
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-09-18 20:05:47 -0500

    Fix ls color wrapper to work on macOS

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,3 +1,12 @@
-function ls --description 'alias ls=ls --color=auto'
-	command ls --color=auto $argv
+# cache the result of this operation so we don't call
+# `uname` each time we execute the function below
+set -l flag
+if uname | string match Darwin >/dev/null
+	set flag -G
+else
+	set flag --color=auto
+end
+
+function ls --wraps ls --inherit-variable flag
+	command ls $flag $argv
 end
