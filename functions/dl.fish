79e8504365334a8091aecd8b96af1049513e5538
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-04 12:05:35 -0500

    fish function descriptions that will actually show
    Courtesy of https://github.com/fish-shell/fish-shell/pull/5218

--- functions/dl.fish
+++ functions/dl.fish
@@ -1,3 +1,3 @@
-function dl --wraps aria2c
+function dl --wraps aria2c --description "download with aria2"
 	aria2c -j8 -x8 --min-split-size=4M --file-allocation=trunc $argv;
 end
--- functions/man.fish
+++ functions/man.fish
@@ -1,5 +1,5 @@
-function man
-	set arg $argv[0..-1]
+function man --description "an nvim wrapper for man"
+	set arg $argv[1..-1]
 	if not command man -w $arg >/dev/null 2>/dev/null
 		echo "No man page is available for '$arg'" 1>&2
 		return 1
