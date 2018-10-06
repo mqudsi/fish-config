ee6139d495996df707718e63b7875317812eacf5
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-05 22:56:47 -0500

    Add `dos2unix` in lemonade paste pipeline, but only if it exists

--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -5,12 +5,19 @@ function pbpaste
 		return $status
 	end
 
-	#lemonade does not understand IPv6!
+	# In case of remote hosts
+	# lemonade does not understand IPv6!
 	if type -q lemonade; and string length -q $SSH_CLIENT; and not string match -rq ':' $SSH_CLIENT
 		set -l host (echo $SSH_CLIENT | cut -f1 -d ' ')
-		lemonade --host $host paste 2>/dev/null
+		lemonade --host $host paste 2>/dev/null | read -lz text
+		if type -q dos2unix
+			echo $text | dos2unix
+		else
+			echo $text
+		end
 		return
 	end
+
 	set -l uname (uname -a)
 	if echo $uname | string match -qr -- "Microsoft"
 		paste.exe <&0 2>/dev/null
