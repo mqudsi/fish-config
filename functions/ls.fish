376c18c3caca3c71055debc9345d4365f358ffa4
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-09 20:13:51 -0500

    Fix ls detection

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,10 +1,11 @@
 # cache the result of this operation so we don't call
 # `uname` each time we execute the function below
 set -l flag
-if uname | string match Darwin >/dev/null
-	set flag -G
-else
+if ls --color=auto /tmp 1>/dev/null 2>/dev/null
 	set flag --color=auto
+else if ls -G /tmp 1>/dev/null 2>/dev/null
+	# macOS and FreeBSD
+	set flag -G
 end
 
 function ls --wraps ls --inherit-variable flag
