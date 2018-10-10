da8845e4bb751811758cbfec4ed7d73c3e469ccd
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-09 20:12:13 -0500

    Update pkg stub

--- functions/pkg.fish
+++ functions/pkg.fish
@@ -1,10 +1,8 @@
 if uname -a | string match -qr "Microsoft|Ubuntu|Debian"
-	complete -c pkg -e
 	function pkg --wraps apt
 		apt $argv
 	end
 else if uname -a | string match -qr "Darwin"
-	complete -c pkg -e
 	function pkg --wraps brew
 		brew $argv
 	end
