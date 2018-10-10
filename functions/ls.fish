29c29e4c3d919978bf32b4f899cbf2c9c0172810
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-10 14:09:05 -0500

    Fix recursive invocation of ls

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,16 +1,16 @@
 # cache the result of this operation so we don't call
 # `uname` each time we execute the function below
 set -l flag
-if ls --color=auto /tmp 1>/dev/null 2>/dev/null
+if command ls --color=auto /tmp 1>/dev/null 2>/dev/null
 	set flag --color=auto
-else if ls -G /tmp 1>/dev/null 2>/dev/null
+else if command ls -G /tmp 1>/dev/null 2>/dev/null
 	# macOS and FreeBSD
 	set flag -G
 end
 
 # recent versions of coreutils broke `ls`. Nope, not kidding.
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=886173
-if ls --quoting-style=literal /tmp >/dev/null 2>/dev/null
+if command ls --quoting-style=literal /tmp >/dev/null 2>/dev/null
 	set flag $flag --quoting-style=literal
 end
 
