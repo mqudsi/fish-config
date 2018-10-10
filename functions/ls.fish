497520f1d08f646ed28cd13d788fc6590d230994
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-09 20:17:43 -0500

    Work around coreutils by-design bug in `ls`
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=886173

--- functions/ls.fish
+++ functions/ls.fish
@@ -8,6 +8,12 @@ else if ls -G /tmp 1>/dev/null 2>/dev/null
 	set flag -G
 end
 
+# recent versions of coreutils broke `ls`. Nope, not kidding.
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=886173
+if ls --quoting-style=literal /tmp >/dev/null 2>/dev/null
+	set flag $flag --quoting-style=literal
+end
+
 function ls --wraps ls --inherit-variable flag
 	command ls $flag $argv
 end
