dfc46030f551d1291e5bfea38a9c26b1875801ee
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-14 19:57:51 -0500

    Fix read content piped to function

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -5,16 +5,18 @@ function pbcopy
 		return $status
 	end
 
+	read -zl contents
+
 	#lemonade does not understand IPv6!
 	if type -q lemonade; and string length -q $SSH_CLIENT; and not string match -rq ':' $SSH_CLIENT
 		set -l host (echo $SSH_CLIENT | cut -f1 -d ' ')
-		lemonade --host $host copy 2>/dev/null
+		printf $contents | lemonade --host $host copy 2>/dev/null
 		return
 	end
 	set -l uname (uname -a)
 	if echo $uname | string match -qr -- "Microsoft"
-		clip.exe <&0 2>/dev/null
+		printf $contents | clip.exe 2>/dev/null
 	else if echo $uname | string match -qr -- "Darwin"
-		command pbcopy <&0 2>/dev/null
+		printf $contents | command pbcopy 2>/dev/null
 	end
 end
