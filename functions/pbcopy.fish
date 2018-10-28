337d7b037758ac77851c1bdc1111625e888cab72
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-10-28 14:05:52 -0500

    Revert "Fix read content piped to function"
    This reverts commit dfc46030f551d1291e5bfea38a9c26b1875801ee.
    `read -z` cannot handle large content (and is very slow to boot).
    
    ref https://github.com/fish-shell/fish-shell/issues/5290

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -5,18 +5,16 @@ function pbcopy
 		return $status
 	end
 
-	read -zl contents
-
 	#lemonade does not understand IPv6!
 	if type -q lemonade; and string length -q $SSH_CLIENT; and not string match -rq ':' $SSH_CLIENT
 		set -l host (echo $SSH_CLIENT | cut -f1 -d ' ')
-		printf $contents | lemonade --host $host copy 2>/dev/null
+		lemonade --host $host copy 2>/dev/null
 		return
 	end
 	set -l uname (uname -a)
 	if echo $uname | string match -qr -- "Microsoft"
-		printf $contents | clip.exe 2>/dev/null
+		clip.exe <&0 2>/dev/null
 	else if echo $uname | string match -qr -- "Darwin"
-		printf $contents | command pbcopy 2>/dev/null
+		command pbcopy <&0 2>/dev/null
 	end
 end
--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -20,8 +20,8 @@ function pbpaste
 
 	set -l uname (uname -a)
 	if echo $uname | string match -qr -- "Microsoft"
-		paste.exe 2>/dev/null
+		paste.exe <&0 2>/dev/null
 	else if echo $uname | string match -qr -- "Darwin"
-		command pbpaste 2>/dev/null
+		command pbpaste <&0 2>/dev/null
 	end
 end
