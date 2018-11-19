9d0be40fbfa63ecf127436bd141d34401085fa13
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-11-18 18:49:54 -0600

    Remove manual \ev binding
    fish 3 has an explicit "edit buffer" command bound to \ev.

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,7 +1,6 @@
 function fish_user_key_bindings
     fzf_key_bindings
 
-    bind \ev 'edit_cmd; commandline -f execute'
     #bind alt+del/bkspc to kill words
     bind \e\e\[3~ kill-word
     bind \e\cH backward-kill-word
