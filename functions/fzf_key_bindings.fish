73987567ef4649097684037beecc39d5fca6c16a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-04-25 15:43:31 -0500

    Check for FZF plugin existence before sourcing

--- functions/fzf_key_bindings.fish
+++ functions/fzf_key_bindings.fish
@@ -2,4 +2,6 @@ if not set -qx GOPATH
 	set -gx GOPATH ~/go
 end
 
-source "$GOPATH/src/github.com/junegunn/fzf/shell/key-bindings.fish"
+if test -f "$GOPATH/src/github.com/junegunn/fzf/shell/key-bindings.fish"
+	source "$GOPATH/src/github.com/junegunn/fzf/shell/key-bindings.fish"
+end
