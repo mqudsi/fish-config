7d1da8e13ea18c25678458d75e49151ec73a42cd
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-12-10 13:30:25 -0600

    Merge consecutive nvim command modifiers in man.fish

--- functions/man.fish
+++ functions/man.fish
@@ -6,10 +6,26 @@ function man --description "an nvim wrapper for man"
 	end
 
 	if type -q nvim
-		nvim -u NORC -c ":set fileignorecase" -c ":Man $arg" -c ":silent :only" \
-			-c ':set mouse=a' \
-			-c 'nunmap <buffer> q' -c 'nmap q :q!<CR>' -c 'nmap <space> <PageDown>' \
-			-c ':call feedkeys("gg")'
+		nvim -u NORC -c ":set fileignorecase
+			:Man $arg" -c ":silent :only" \
+			-c ':nunmap <buffer> q
+			:nmap q :q!<CR>
+			:nmap <space> <PageDown>' \
+			# use `q` to exit like in `less`
+			#-c ':<silent> nunmap <buffer> q
+			-c ':nmap q :q!<CR>' \
+			# # map `space` to `PageDown`
+			-c 'nmap <space> <PageDown>' \
+			# # use up/down keys to navigate even soft-wrapped lines
+			-c 'nnoremap <Up> g<Up>
+			:nnoremap <Down> g<Down>
+			:nnoremap <silent> <Home> g<Home>
+			:nnoremap <silent> <End> g<End>
+			:inoremap <silent> <Home> <c-o>g<Home>
+			:inoremap <silent> <End> <c-o>g<End>' \
+			# # for some reason, it starts scrolled to the bottom so scroll to top
+			-c ':call feedkeys("gg")
+			set mouse=a'
 	else
 		command man $arg
 	end
