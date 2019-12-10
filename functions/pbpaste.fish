8cad80bfcf473265e85b1e111c535cf24a3398e5
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-12-10 13:31:29 -0600

    Use --lf to paste unix endings from paste.exe

--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -20,7 +20,7 @@ function pbpaste
 
 	set -l uname (uname -a)
 	if echo $uname | string match -qr -- "Microsoft"
-		paste.exe <&0 2>/dev/null
+		paste.exe --lf <&0 2>/dev/null
 	else if echo $uname | string match -qr -- "Darwin"
 		command pbpaste <&0 2>/dev/null
 	end
