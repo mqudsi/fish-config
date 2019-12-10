8828faa8dad88bad0305f9b7ed7b8a852000d06e
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-12-10 13:32:21 -0600

    Fix nuget completions

--- functions/nuget.fish
+++ functions/nuget.fish
@@ -1,6 +1,7 @@
-function nuget 
-	if echo $argv | egrep "\bpush\b" >/dev/null
-		set argv $argv -source https://nuget.org/
-	end
+function nuget
+	# This isn't needed, just run `nuget config -Set defaultPushSource https://nuget.org/`
+	# if echo $argv | egrep "\bpush\b" >/dev/null
+	# 	set argv $argv -source https://nuget.org/
+	# end
 	nuget.exe $argv
 end
