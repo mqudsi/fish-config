f0b267cc267dd4b875c1a276fe326498f0fb187f
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2021-09-29 12:27:15 -0500

    Swallow trailing new lines in paste output

--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -19,9 +19,12 @@ function pbpaste
 	end
 
 	set -l uname (uname -a)
+	# set -l output (
 	if echo $uname | string match -qr -- "Microsoft"
 		paste.exe --lf <&0 2>/dev/null
 	else if echo $uname | string match -qr -- "Darwin"
 		command pbpaste <&0 2>/dev/null
 	end
+	# )
+	# printf $output
 end
