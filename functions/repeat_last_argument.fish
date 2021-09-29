29462529c9078cf73944fe3294a7cd9253c3f318
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2021-09-29 12:25:38 -0500

    repeat_last_argument: escape the argument

--- functions/repeat_last_argument.fish
+++ functions/repeat_last_argument.fish
@@ -1,5 +1,5 @@
 function repeat_last_argument
-	set -l last (commandline -o)[-1]
+	set -l last (string escape -- (commandline -co)[-1])
 	commandline -a $last
 	commandline -C (string length (commandline))
 end
