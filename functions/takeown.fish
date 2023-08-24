7b52584c30243a5cc881c1a0d151ff201c379667
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-08-24 10:37:36 -0500

    Remove trailing backslash in `takeown` to make more extensible

--- functions/takeown.fish
+++ functions/takeown.fish
@@ -1,5 +1,5 @@
 # Defined in - @ line 0
 function takeown --description 'alias takeown sudo chown (whoami):(whoami) -R'
 	# sudo chown -R (whoami):(whoami) $argv;
-	sudo chown -R (whoami) $argv;
+	sudo chown -R (whoami) $argv
 end
