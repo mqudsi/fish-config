124c65cb74f1f05798feb792924f11918b2ddc6e
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-10-07 10:36:29 -0500

    ssh2.fish: s/exit/return/

--- functions/ssh2.fish
+++ functions/ssh2.fish
@@ -1,11 +1,11 @@
 function ssh2
 	if ! string match -qr "^(?P<user>.*)@(?P<host>.*)" $argv[-1]
 		echo "Error matching username and host!" >&2;
-		exit 1
+		return 1
 	end
 	if ! set host (dig @192.168.45.1 +short $host)
 		echo "Error looking up ip of $host" >&2
-		exit 1
+		return 1
 	end
 
 	ssh $argv[..-2] $user@$host
