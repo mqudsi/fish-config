d5204524cf2d26a2f7c8b4f9247394afc08d41ca
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-10-10 11:33:01 -0500

    Update `pbcopy` and `pbpaste` to support `xclip`
    Also no longer run `uname -a` on each invocation.

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -1,20 +1,23 @@
+if not set -q __uname_a
+	set -g __uname_a (uname -a)
+end
+
 function pbcopy
-	#pbcopy FILE can be used to copy contents of a file
+	# `pbcopy FILE` can be used to copy contents of a file
 	if set -q argv[1]
 		cat $argv[1] | pbcopy
 		return $status
 	end
 
-	#lemonade does not understand IPv6!
+	# `lemonade` does not understand IPv6!
 	if type -q lemonade; and string length -q $SSH_CLIENT; and not string match -rq ':' $SSH_CLIENT
 		set -l host (echo $SSH_CLIENT | cut -f1 -d ' ')
 		lemonade --host $host copy 2>/dev/null
-		return
-	end
-	set -l uname (uname -a)
-	if echo $uname | string match -qr -- "Microsoft"
+	else if echo $__uname_a | string match -qr -- "Microsoft"
 		clip.exe <&0 2>/dev/null
-	else if echo $uname | string match -qr -- "Darwin"
+	else if echo $__uname_a | string match -qr -- "Darwin"
 		command pbcopy <&0 2>/dev/null
+	else if type -q xclip
+		xclip -selection clipboard -i <&0
 	end
 end
--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -1,12 +1,16 @@
+if not set -q __uname_a
+	set -g __uname_a (uname -a)
+end
+
 function pbpaste
-	# pbpaste FILE can be used to paste to a file
+	# `pbpaste FILE` can be used to paste to a file
 	if set -q argv[1]
 		pbpaste > $argv[1]
 		return $status
 	end
 
 	# In case of remote hosts
-	# lemonade does not understand IPv6!
+	# `lemonade` does not understand IPv6!
 	if type -q lemonade; and string length -q $SSH_CLIENT; and not string match -rq ':' $SSH_CLIENT
 		set -l host (echo $SSH_CLIENT | cut -f1 -d ' ')
 		lemonade --host $host paste 2>/dev/null | read -lz text
@@ -15,16 +19,11 @@ function pbpaste
 		else
 			echo $text
 		end
-		return
-	end
-
-	set -l uname (uname -a)
-	# set -l output (
-	if echo $uname | string match -qr -- "Microsoft"
+	else if echo $__uname_a | string match -qr -- "Microsoft"
 		paste.exe --lf <&0 2>/dev/null
-	else if echo $uname | string match -qr -- "Darwin"
+	else if echo $__uname_a | string match -qr -- "Darwin"
 		command pbpaste <&0 2>/dev/null
+	else if type -q xclip
+		xclip -selection clipboard -o
 	end
-	# )
-	# printf $output
 end
