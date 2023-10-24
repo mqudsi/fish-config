9dded0cb1e5d1e27497fcfe5a1b43baf547936fb
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-10-24 12:53:05 -0500

    extract_subs: Add support for specifying index

--- functions/extract_subs.fish
+++ functions/extract_subs.fish
@@ -1,7 +1,12 @@
 function extract_subs
-set out (path change-extension .srt $argv[1])
-if set -q argv[2]
-set out $argv[2]
-end
-ffmpeg -i $argv[1] $out
+	set out (path change-extension .srt $argv[1])
+	set idx 0
+	if string match -q -- $argv[2] "--index"
+		set idx $argv[3]
+		set argv $argv[1] $argv[4..]
+	end
+	if set -q argv[2]
+		set out $argv[2]
+	end
+	ffmpeg -i $argv[1] -map 0:s:$idx $out
 end
