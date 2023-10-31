2d0ce347f702e9499d513470f9ba71db1c5468c3
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-10-31 12:13:27 -0500

    open: evaluate options in a better order
    We want to use open.exe on WSL, so check for that before xdg-open, which will be
    available but won't do what we want.

--- functions/open.fish
+++ functions/open.fish
@@ -1,13 +1,13 @@
-if command -q open
-	function open
-		for target in $argv
-			command open $target
-		end
-	end
-else if command -q open.exe
+if command -q open.exe
 	function open
 		for target in $argv
 			command open.exe $target
+		end
+	end
+else if command -q xdg-open
+	function open
+		for target in $argv
+			command xdg-open $target
 		end
 	end
 end
