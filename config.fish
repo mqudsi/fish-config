9055ae3ea6aa5b4303979e55976988804b9eb57f
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2024-05-30 17:09:01 -0500

    Add WSL drive abbreviation

--- config.fish
+++ config.fish
@@ -26,19 +26,40 @@ set -x CLICOLOR
 # https://github.com/fish-shell/fish-shell/commit/c02bf2548f6e4cc6283a6716006705c6353bd41d
 bind \cc 'commandline -f cancel; commandline ""'
 
-# Map ctrl-n to launch a new terminal window; useful for terminals that don't
-# do so by default. This should work anywhere `open` exists.
-if type -q open
-	bind \cN 'open fish'
-end
-
 # Stop posting junk to the terminal when INS is pressed
 bind -k ic true
 
-if type -q git.exe
-	function git.exe --wraps git
-		command git.exe $argv
-	end
-end
-
 set -g MOVFLAGS -movflags +write_colr+use_metadata_tags+frag_keyframe+empty_moov+default_base_moof+faststart -frag_duration 1000000
+
+abbr sudo 'sudo -E'
+# For some reason on my Model M keyboard I find myself often typing `gitp ull` instead of `git pull`
+abbr --add gitp --set-cursor='!' 'git p!'
+
+if string match -qei microsoft <?/proc/version
+	# Map ctrl-n to launch a new terminal window; useful for terminals that don't
+	# do so by default. This should work anywhere `open` exists.
+	type -q open; and bind \cN 'open fish'
+
+	function __wsl_drive
+		function __mount_wsl_drive
+			set letter (string match -r '^[A-Za-z]' -- $argv[1] | string lower)
+			or echo "Invalid drive parameter!" && return -1
+
+			if mount | string match -irq $letter:
+				echo "Already mounted!"
+				return 0
+			end
+
+			set path /mnt/$letter
+			sudo -E mkdir -p $path
+			set drive (printf '%c:'\\ (string upper -- $letter))
+			sudo -E mount -t drvfs -o metadata,rw,noatime,uid=1000,gid=1000,umask=22,fmask=11,metadata,case=dir $drive $path
+		end
+
+		set letter (string match -r '^[A-Za-z]' -- $argv[1] | string lower)
+		__mount_wsl_drive $letter 2>/dev/null 1>/dev/null
+		and cd /mnt/$letter
+	end
+
+	abbr -a wsl_drive --position command --regex '^[a-zA-Z]:$' --function __wsl_drive
+end
